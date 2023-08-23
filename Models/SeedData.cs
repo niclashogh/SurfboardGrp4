6492de7448fbe1a4bfe82668e08b9bb5222e0159
Christoffer Jansson <christofferjansson1@gmail.com>
2023-08-23 21:42:29 +0200

    add all seedData from assigment

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -71,7 +71,89 @@ namespace SurfboardGrp4.Models
                         Price = 645,
                         Equipment = "",
                         ImgUrl = ""
+                    },
+
+
+                    new Board
+                    {
+                        Name = "The Emerald Glider",
+                        Length = 9.2,
+                        Width = 22.8,
+                        Thickness = 2.8,
+                        Volume = 65.4,
+                        Type = "Longboard",
+                        Price = 895,
+                        Equipment = "",
+                        ImgUrl = ""
+                    },
+
+                    new Board
+                    {
+                        Name = "The Bomb",
+                        Length = 5.5,
+                        Width = 21,
+                        Thickness = 2.5,
+                        Volume = 33.7,
+                        Type = "Shortboard",
+                        Price = 645,
+                        Equipment = "",
+                        ImgUrl = ""
+                    },
+
+
+                    new Board
+                    {
+                        Name = "Walden Magic",
+                        Length = 9.6,
+                        Width = 19.4,
+                        Thickness = 3,
+                        Volume = 80,
+                        Type = "Longboard",
+                        Price = 1025,
+                        Equipment = "",
+                        ImgUrl = ""
+                    },
+
+                    new Board
+                    {
+                        Name = "Naish One",
+                        Length = 12.6,
+                        Width = 30,
+                        Thickness = 6,
+                        Volume = 301,
+                        Type = "SUB",
+                        Price = 854,
+                        Equipment = "Paddle",
+                        ImgUrl = ""
+                    },
+
+                    new Board
+                    {
+                        Name = "Six Tourer",
+                        Length = 11.6,
+                        Width = 32,
+                        Thickness = 6,
+                        Volume = 270,
+                        Type = "SUB",
+                        Price = 611,
+                        Equipment = "Fin, Paddle, Pump, Leash",
+                        ImgUrl = ""
+                    },
+
+                    new Board
+                    {
+                        Name = "Naish Maliko",
+                        Length = 14,
+                        Width = 25,
+                        Thickness = 6,
+                        Volume = 330,
+                        Type = "SUB",
+                        Price = 1304,
+                        Equipment = "Fin, Paddle, Pump, Leash",
+                        ImgUrl = ""
                     }
+
+
                 );
                 context.SaveChanges();
             }

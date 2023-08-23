23de81f1d3eccb46bfdce71e3c434ffc45a79d9a
Niclas Høgh <niclas.hogh@gmail.com>
2023-08-23 12:18:03 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SurfboardGrp4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

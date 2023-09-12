ce575ec723a00e82b58c374827e958e0ddeefd18
Niclas Høgh <niclas.hogh@gmail.com>
2023-09-12 11:36:21 +0200

    Refresh

---
+++ Controllers/RentalsController.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SurfboardGrp4.Data;
+using SurfboardGrp4.Models;
+
+namespace SurfboardGrp4.Controllers
+{
+    public class RentalsController : Controller
+    {
+        private readonly SurfboardGrp4Context _context;
+
+        public RentalsController(SurfboardGrp4Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Rentals
+        public async Task<IActionResult> Index()
+        {
+            var surfboardGrp4Context = _context.Rental.Include(r => r.Board);
+            return View(await surfboardGrp4Context.ToListAsync());
+        }
+
+        // GET: Rentals/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Rental == null)
+            {
+                return NotFound();
+            }
+
+            var rental = await _context.Rental
+                .Include(r => r.Board)
+                .FirstOrDefaultAsync(m => m.RentalID == id);
+            if (rental == null)
+            {
+                return NotFound();
+            }
+
+            return View(rental);
+        }
+
+        // GET: Rentals/Create
+        public IActionResult Create()
+        {
+            ViewData["BoardID"] = new SelectList(_context.Board, "ID", "ID");
+            return View();
+        }
+
+        // POST: Rentals/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserID,RentalID,BoardID,StartDate,EndDate,TotalPrice")] Rental rental)
+        {
+            rental.BoardID = 1;
+            rental.RentalID = 1;
+            rental.TotalPrice = 9000;
+            rental.UserID = 0;
+
+            if (ModelState.IsValid)
+            {
+
+                _context.Add(rental);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BoardID"] = new SelectList(_context.Board, "ID", "ID", rental.BoardID);
+            return View(rental);
+        }
+
+        // GET: Rentals/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Rental == null)
+            {
+                return NotFound();
+            }
+
+            var rental = await _context.Rental.FindAsync(id);
+            if (rental == null)
+            {
+                return NotFound();
+            }
+            ViewData["BoardID"] = new SelectList(_context.Board, "ID", "ID", rental.BoardID);
+            return View(rental);
+        }
+
+        // POST: Rentals/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("UserID,RentalID,BoardID,StartDate,EndDate,TotalPrice")] Rental rental)
+        {
+            if (id != rental.RentalID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(rental);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RentalExists(rental.RentalID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BoardID"] = new SelectList(_context.Board, "ID", "ID", rental.BoardID);
+            return View(rental);
+        }
+
+        // GET: Rentals/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Rental == null)
+            {
+                return NotFound();
+            }
+
+            var rental = await _context.Rental
+                .Include(r => r.Board)
+                .FirstOrDefaultAsync(m => m.RentalID == id);
+            if (rental == null)
+            {
+                return NotFound();
+            }
+
+            return View(rental);
+        }
+
+        // POST: Rentals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Rental == null)
+            {
+                return Problem("Entity set 'SurfboardGrp4Context.Rental'  is null.");
+            }
+            var rental = await _context.Rental.FindAsync(id);
+            if (rental != null)
+            {
+                _context.Rental.Remove(rental);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RentalExists(int id)
+        {
+          return (_context.Rental?.Any(e => e.RentalID == id)).GetValueOrDefault();
+        }
+
+        public IActionResult Book()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Book([Bind("BoardID,RentalID,UserID,StartDate,EndDate,TotalPrice")] Rental rental)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _context.Add(rental);
+        //        await _context.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(rental);
+        //}
+    }
+}
---
+++ Models/Rental.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SurfboardGrp4.Models
+{
+    public class Rental
+    {
+        //[ForeignKey(Board.ID)]
+        public Board? Board { get; set; }
+        public int UserID { get; set; }
+        public int RentalID { get; set; }
+        public int BoardID { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public double TotalPrice { get; set; }
+
+    }
+}
